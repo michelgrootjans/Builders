b7d47e10b7c0d1e115ab83365f4a3058da431e20
Michel Grootjans <michel.grootjans@gmail.com>
2014-05-01 11:56:32 +0200

    cleanup

--- src/Builders.Tests/WithPlant/RelationshipTests.cs
+++ src/Builders.Tests/WithPlant/RelationshipTests.cs
@@ -20,14 +20,6 @@ namespace Builders.Tests.WithPlant
         }
     }
 
-    public class HouseVariationsBlueprint : IBlueprint
-    {
-        public void SetupPlant(BasePlant p)
-        {
-            p.DefineVariationOf<House>("appartment", new { Color = "blue" });
-        }
-    }
-
     public class RelationshipTests
     {
         private BasePlant plant;
@@ -44,27 +36,6 @@ namespace Builders.Tests.WithPlant
             var person = plant.Create<Person>();
             Assert.That(person.Name, Is.EqualTo("my name"));
             Assert.That(person.House.Color, Is.EqualTo("red"));
-        }
-    }
-
-    public class VariationTests
-    {
-        private BasePlant plant;
-
-        [SetUp]
-        public void SetUp()
-        {
-            plant = new BasePlant().WithBlueprintsFromAssemblyOf<VariationTests>();
-        }
-
-        [Test]
-        public void Variation()
-        {
-            var house1 = plant.Create<House>();
-            var house2 = plant.Create<House>(variation: "appartment");
-
-            Assert.That(house1.Color, Is.EqualTo("red"));
-            Assert.That(house2.Color, Is.EqualTo("blue"));
         }
     }
 }
