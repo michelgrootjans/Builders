7a5e1048445f9d648bacb5a74fde0976a0bae6d2
Michel Grootjans <michel.grootjans@gmail.com>
2014-05-01 11:55:09 +0200

    added relationships and variations to exapmples

---
+++ src/Builders.Tests/Entities/Person.cs
@@ -0,0 +1,13 @@
+﻿namespace Builders.Tests.Entities
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public House House { get; set; }
+    }
+
+    public class House
+    {
+        public string Color { get; set; }
+    }
+}
---
+++ src/Builders.Tests/WithPlant/RelationshipTests.cs
@@ -0,0 +1,70 @@
+﻿using Builders.Tests.Entities;
+using NUnit.Framework;
+using Plant.Core;
+
+namespace Builders.Tests.WithPlant
+{
+    public class PersonBlueprint : IBlueprint
+    {
+        public void SetupPlant(BasePlant p)
+        {
+            p.DefinePropertiesOf<Person>(new {Name = "my name"});
+        }
+    } 
+
+    public class HouseBlueprint : IBlueprint
+    {
+        public void SetupPlant(BasePlant p)
+        {
+            p.DefinePropertiesOf<House>(new {Color = "red"});
+        }
+    }
+
+    public class HouseVariationsBlueprint : IBlueprint
+    {
+        public void SetupPlant(BasePlant p)
+        {
+            p.DefineVariationOf<House>("appartment", new { Color = "blue" });
+        }
+    }
+
+    public class RelationshipTests
+    {
+        private BasePlant plant;
+
+        [SetUp]
+        public void SetUp()
+        {
+            plant = new BasePlant().WithBlueprintsFromAssemblyOf<RelationshipTests>();
+        }
+
+        [Test]
+        public void SimpleRelationship()
+        {
+            var person = plant.Create<Person>();
+            Assert.That(person.Name, Is.EqualTo("my name"));
+            Assert.That(person.House.Color, Is.EqualTo("red"));
+        }
+    }
+
+    public class VariationTests
+    {
+        private BasePlant plant;
+
+        [SetUp]
+        public void SetUp()
+        {
+            plant = new BasePlant().WithBlueprintsFromAssemblyOf<VariationTests>();
+        }
+
+        [Test]
+        public void Variation()
+        {
+            var house1 = plant.Create<House>();
+            var house2 = plant.Create<House>(variation: "appartment");
+
+            Assert.That(house1.Color, Is.EqualTo("red"));
+            Assert.That(house2.Color, Is.EqualTo("blue"));
+        }
+    }
+}
